f0f06b55e3c70ff1a894946f56fcf61576fede12
Faried <87261798+TheDevN00b@users.noreply.github.com>
2024-07-24 15:29:06 +0700

    Add files via upload

---
+++ Blog/script.js
@@ -0,0 +1,96 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
+    body.classList.add('no-transition');
+
+    const savedTheme = localStorage.getItem('darkMode');
+
+    if (savedTheme === 'true') {
+        applyDarkMode(false);
+    } else {
+        applyLightMode(false);
+    }
+
+    setTimeout(() => {
+        body.classList.remove('no-transition');
+    }, 0);
+});
+
+function toggleDarkMode() {
+    const body = document.querySelector('body');
+    if (body.classList.contains('dark-mode')) {
+        applyLightMode(true);
+    } else {
+        applyDarkMode(true);
+    }
+}
+
+function applyDarkMode(withTransition) {
+    const body = document.querySelector('body');
+    const h1 = document.querySelector('.perkenalan');
+    const h1_a = document.querySelector('.perkenalan a');
+    const h2 = document.querySelector('.add');
+    const p = document.querySelector('.desc');
+    const p_a = document.querySelector('.desc a');
+    const menuLinks = document.querySelectorAll('.nav_link');
+
+    if (!withTransition) body.classList.add('no-transition');
+
+    body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'true');
+    document.getElementById('theme').textContent = 'Theme';
+    h1.style.color = 'white';
+    h1_a.style.color = 'white';
+    h2.style.color = 'white';
+    p.style.color = 'white';
+    p_a.style.color = 'white';
+    menuLinks.forEach(link => {
+        link.style.color = 'white';
+    });
+
+    if (!withTransition) setTimeout(() => body.classList.remove('no-transition'), 0);
+}
+
+function applyLightMode(withTransition) {
+    const body = document.querySelector('body');
+    const h1 = document.querySelector('.perkenalan');
+    const h1_a = document.querySelector('.perkenalan a');
+    const h2 = document.querySelector('.add');
+    const p = document.querySelector('.desc');
+    const p_a = document.querySelector('.desc a');
+    const menuLinks = document.querySelectorAll('.nav_link');
+
+    if (!withTransition) body.classList.add('no-transition');
+
+    body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'false');
+    document.getElementById('theme').textContent = 'Theme';
+    h1.style.color = '';
+    h1_a.style.color = '';
+    h2.style.color = '';
+    p.style.color = '';
+    p_a.style.color = '';
+    menuLinks.forEach(link => {
+        link.style.color = '';
+    });
+
+    if (!withTransition) setTimeout(() => body.classList.remove('no-transition'), 0);
+}
+
+const projectsLink = document.querySelector('.nav_link[href="#projects"]');
+
+const popup = document.getElementById('popup');
+const popupContent = document.getElementById('popupContent');
+
+function openPopup(content) {
+    popupContent.textContent = content;
+    popup.style.display = 'block';
+}
+
+projectsLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    openPopup("SOON!");
+});
+
+document.getElementById('closeBtn').addEventListener('click', () => {
+    popup.style.display = 'none';
+});
